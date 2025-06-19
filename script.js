116db455e9d93c5c725076eb10691def07de42ae
n-asma1 <asmahusna150105@gmail.com>
2025-06-19 09:37:43 +0530

    file added

---
+++ script.js
@@ -0,0 +1,26 @@
+//function
+let a=10;
+function ab(){
+    var b=10;
+    console.log(a)
+    console.log(b)
+}
+//block
+let a1=20;
+{
+ var b=20;
+ console.log(a)
+    console.log(b)
+}
+console.log(b)
+
+//thruthy vs falsy
+if(undefined){
+    console.log(1)
+}
+else{
+    console.log(2)
+}
+
+
+
